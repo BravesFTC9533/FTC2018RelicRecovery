5251d93ea78427ad4a068bb34a2993c34124a07d
ftc9533mm <ftc9533mm@gmail.com>
2017-10-28 15:25:01 -0400

    Added Config class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+/**
+ * Created by 9533 on 10/28/2017.
+ */
+//save & write methods
+    //red & blue enums
+    //front & back enums
+    //booleans for park
+public class Config {
+
+
+    public void Save(){
+
+    }
+
+    public void Write(){
+
+    }
+
+
+    public enum Color{
+        RED, BLUE
+
+
+
+
+    }
+
+    public enum Position{
+         FRONT,
+        BACK
+
+
+
+
+    }
+
+    public boolean Park;
+    public boolean JewelKnockOff;
+    public boolean CryptoBox;
+
+
+
+
+
+
+
+
+
+
+}
