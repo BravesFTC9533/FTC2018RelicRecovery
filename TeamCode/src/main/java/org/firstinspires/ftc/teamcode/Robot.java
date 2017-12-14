45b01fac9cd1ea33f90e46741ac438557e35fb85
AmazingSpanoMan <TheSpanoMan@gmail.com>
2017-12-14 14:59:39 -0500

    Code Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -23,6 +24,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
+import javax.sql.RowSetEvent;
+
 /**
  * Created by dmill on 10/28/2017.
  */
@@ -32,10 +35,13 @@ public class Robot {
 
     public DcMotor motorLeft = null;
     public DcMotor motorRight = null;
+    //public RevRoboticsCoreHexMotor motorLeft = null;
+    //public RevRoboticsCoreHexMotor motorRight = null;
 
     //public DcMotor relicArmExtender = null;
 
     public DcMotor motorLift = null;
+    //public RevRoboticsCoreHexMotor motorLift = null;
 
 
 
