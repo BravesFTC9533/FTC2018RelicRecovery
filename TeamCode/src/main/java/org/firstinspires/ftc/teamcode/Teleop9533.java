70a71766b8b99255543944069992d9e6a2ed5d27
madikasu <douglas.miller@gmail.com>
2018-02-03 17:52:17 -0500

    Remove unneeded BACK code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop9533.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop9533.java
@@ -155,9 +155,7 @@ public class Teleop9533 extends LinearOpMode9533 implements FtcGamePad.ButtonHan
                 }
                 break;
             case FtcGamePad.GAMEPAD_BACK:
-                if(pressed){
 
-                }
                 break;
 
         }
