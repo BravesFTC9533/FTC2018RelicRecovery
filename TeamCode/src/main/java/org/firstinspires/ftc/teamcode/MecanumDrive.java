60cdd71799c14419e3a175f475b2a5fe4b09f0f6
madikasu <douglas.miller@gmail.com>
2018-02-03 20:12:54 -0500

    Fill out mechanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.teamcode.RobotV2.RobotV2;
 
 /**
@@ -27,6 +29,80 @@ public class MecanumDrive implements IDrive {
     }
 
     public void handle(){
-        robot.driveForward();
+
+        //mechDrive.Drive(-gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        double h, v, r;
+
+        h = -driverGamepad.getLeftStickX();
+        v = -driverGamepad.getLeftStickY();
+        r = driverGamepad.getRightStickX();
+
+
+
+        if(getIsReverse()) {
+            h *= -1;
+            v *= -1;
+        }
+
+
+        h = clipMotorPower(h);
+        v = clipMotorPower(v);
+        r = clipMotorPower(r);
+
+        // add vectors
+        double frontLeft =  v-h+r;
+        double frontRight = v+h-r;
+        double backRight =  v-h-r;
+        double backLeft =   v+h+r;
+
+        // since adding vectors can go over 1, figure out max to scale other wheels
+        double max = Math.max(
+                Math.abs(backLeft),
+                Math.max(
+                        Math.abs(backRight),
+                        Math.max(
+                                Math.abs(frontLeft), Math.abs(frontRight)
+                        )
+                )
+        );
+        // only need to scale power if max > 1
+        if(max > 1){
+            frontLeft = scalePower(frontLeft, max);
+            frontRight = scalePower(frontRight, max);
+            backLeft = scalePower(backLeft, max);
+            backRight = scalePower(backRight, max);
+        }
+
+
+
+    }
+
+
+    // Scale motor power based on the max for all wheels
+    // 1, 1, 1, 3 will become .33, .33, .33, 1
+    public static double scalePower(double value, double max){
+        if(max == 0){return  0;}
+        return  value / max;
+    }
+
+    // motor power clipping helper
+    public static double clipMotorPower(double value){
+        return Range.clip(value, -1, 1);
+    }
+
+    public static double scale(double power){
+        int modifier = 1;
+
+        if (power == 0 )
+        {
+            return 0;
+        }
+
+        if(power < 0){
+            modifier *= -1;
+        }
+
+        return  (power * power * modifier);
     }
 }
